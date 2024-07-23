426b692d6f485d922376bf60277aa90a4a014a63
Aayush stha <sudipk339@gmail.com>
2024-07-23 20:36:05 +0545

    NEW

---
+++ images/index.php
@@ -0,0 +1,257 @@
+<php
+
+?>
+
+<?php
+include('header.php')
+?>
+
+
+<body>
+
+    <!-- navbar -->
+    <!-- Navbar -->
+
+  <!-- Navbar -->
+
+        <!-- first body -->
+  <div class="d-flex" style="height: 850px; background: linear-gradient(48deg, green 53%, yellow 47%);">
+            <div class="container-fluid my-auto">
+                <div class="row">
+                    <div class="col-lg-6 text-white">
+                        <h1 class="display-3 text-white">Itahari Namuna College</h1>
+                        <p>Itahari Namuna College (INC) was established in 2070 B.S. in the heart of Itahari Sub-metropolitan City by the expert team of academicians in the town with an aim to be the leading educational institution in its vicinity.
+                             The college has run different streams in Bachelor's level under an umbrella of beautiful, cool and cozy environment in its premises with high-tech infrastructural setup. At present, the college is offering graduate programs on BASW, BCA & BHM, affiliated to Tribhuwan University. 
+                             The college has earned its excellence through its well qualified teaching faculties, innovative teaching pedagogy, transparent and scientific evaluation methods with modern and constantly updated new and professional streams from its inception.</p>
+                             <a href="" class="btn btn-lg btn-primary"> Call for More</a>
+                    </div>
+                    <div class="col-lg-6">
+                
+                    <div class="col-lg-5 mx-auto card shadow-lg">
+                        <div class="card-body">
+                            <h1>Admission Form</h1>
+                            <form action="" method="post">
+                                <div class="form-outline">
+                                     <input type="text" id="typeText" class="form-control" />
+                                     <label class="form-label" for="typeText">Enter your Name</label>
+                                </div>
+                                <br>
+                                 <div class="form-outline">
+                                    <input type="email" id="typeEmail" class="form-control" />
+                                    <label class="form-label" for="typeEmail">enter your email</label>
+                                  </div>
+                                  <br>
+                                  <div class="form-outline">
+                                    <input type="tel" id="typePhone" class="form-control" />
+                                    <label class="form-label" for="typePhone">Enter your mobile number</label>
+                                  </div>
+                                  <br>
+                                  <div class="form-outline">
+                                    <input type="tel" id="faculty" class="form-control" />
+                                    <label class="form-label" for="faculty">faculty</label>
+                                  </div><br>
+                                  <button class="btn btn-primary btn-block">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>     
+  </div>
+        <!-- first body -->
+
+  <section class="py-5">
+    <div class="text-center mb-5"> 
+        <h2 style="font-weight: bold;">Our Courses</h2>
+        <p class="texr-muted">
+            We offer different types of Courses. In the Eastern region we provide the different types of faculties. 
+        </p>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 mb-6">
+                <div class="card">
+                    <div>
+                        <img src="images/Tribhuvan_University_26122014_016.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">
+                            <b> BCA</b>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card">
+                    <div>
+                        <img src="images/Tribhuvan_University_26122014_016.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">
+                            <b> BBS</b>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card">
+                    <div>
+                        <img src="images/Tribhuvan_University_26122014_016.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">
+                            <b> BHM</b>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card">
+                    <div>
+                        <img src="images/Tribhuvan_University_26122014_016.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">
+                            <b> BSW</b>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card">
+                    <div>
+                        <img src="images/Tribhuvan_University_26122014_016.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">
+                            <b> BA</b>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card">
+                    <div>
+                        <img src="images/Tribhuvan_University_26122014_016.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">
+                            <b> BBA</b>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card">
+                    <div>
+                        <img src="images/Tribhuvan_University_26122014_016.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">
+                            <b> MBA</b>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card">
+                    <div>
+                        <img src="images/Tribhuvan_University_26122014_016.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">
+                            <b> +2</b>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </section>
+  <section class="py-5" style="background: gray;">
+    <div class="text-center mb-5"> 
+        <h2 style="font-weight: bold;">Our Staffs</h2>
+        <p class="texr-muted">
+            we collect enthuastic and experiedced staffs with good qualification.
+        </p>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="card">
+                    <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                      <img src="images/teacher.jpeg" class="img-fluid"/>
+                      <a href="#!">
+                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                      </a>
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">Teacher</h5>
+                      <p class="card-text">BCA Hod Department Teacher</p>
+                      <a href="#!" class="btn btn-primary">View More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card">
+                    <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                      <img src="images/teacher.jpeg" class="img-fluid"/>
+                      <a href="#!">
+                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                      </a>
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">Teacher</h5>
+                      <p class="card-text">BHM Hod Department Teacher</p>
+                      <a href="#!" class="btn btn-primary">View More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card">
+                    <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                      <img src="images/teacher.jpeg" class="img-fluid"/>
+                      <a href="#!">
+                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                      </a>
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">Teacher</h5>
+                      <p class="card-text">BASW Hod Department Teacher</p>
+                      <a href="#!" class="btn btn-primary">View More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card" style="height: 500px;">
+                    <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                      <img src="images/teacher.jpeg" class="img-fluid"/>
+                      <a href="#!">
+                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                      </a>
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">Teacher</h5>
+                      <p class="card-text">BBA/BBS/MBA Hod Department Teacher</p>
+                      <a href="#!" class="btn btn-primary">View More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </section>
+
+  <?php
+include('footer.php')
+?>
+
+    <!-- js code -->
+    <!-- MDB -->
+<script
+type="text/javascript"
+src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.js"
+></script>
+</body>
+
+</html>
